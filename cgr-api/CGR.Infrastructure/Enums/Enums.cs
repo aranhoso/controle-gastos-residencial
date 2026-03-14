99e1af2821510823f008be981ae61bcc4db389eb
Guilherme F. <fernandes.guilherme180402@gmail.com>
2026-03-14 14:42:18 -0500

    feat(api): criar estrutura para Enums

---
+++ cgr-api/CGR.Infrastructure/Enums/Enums.cs
@@ -0,0 +1,14 @@
+namespace CGR.Domain.Enums;
+
+public enum TipoTransacao
+{
+    Receita = 1,
+    Despesa = 2
+}
+
+public enum FinalidadeCategoria
+{
+    Receita = 1,
+    Despesa = 2,
+    Ambas = 3
+}
