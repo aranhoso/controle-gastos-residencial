9f1ee1fca76bb978df52d4939ecb131d20672829
Guilherme F. <fernandes.guilherme180402@gmail.com>
2026-03-15 11:09:53 -0500

    feat(api): atualizar configuração do Swagger e adicionar novos serviços e repositórios

--- cgr-api/CGR.Api/Program.cs
+++ cgr-api/CGR.Api/Program.cs
@@ -1,9 +1,27 @@
+using CGR.Application.Interfaces;
+using CGR.Application.Services;
+using CGR.Domain.Interfaces;
 using CGR.Infrastructure.Context;
+using CGR.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("A connection string 'DefaultConnection' não foi encontrada.")));
+
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<IPessoaRepository, PessoaRepository>();
+builder.Services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<IPessoaService, PessoaService>();
+builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 
 builder.Services.AddCors(options =>
 {
@@ -23,11 +41,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseCors("ReactPolicy");
+
+app.MapControllers();
 
 app.Run();
